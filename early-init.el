bb07e465f0b8810c25b6e28a8d09fba46112ffed
李渊 <liyuan@shidegroup.com>
2025-11-24 10:07:02 +0800

    更新init early

--- early-init.el
+++ early-init.el
@@ -43,10 +43,11 @@
 ;; Prevent unwanted runtime compilation for gccemacs (native-comp) users;
 ;; packages are compiled ahead-of-time when they are installed and site files
 ;; are compiled when gccemacs is installed.
-(setq native-comp-deferred-compilation t;; 自 Emacs 29.1 起已废弃
-      native-comp-jit-compilation t     ; 启用即时编译
-      native-comp-always-compile t
+(setq native-comp-deferred-compilation nil
+      native-comp-jit-compilation nil     ; 启用即时编译
+      native-comp-always-compile nil
       native-comp-speed 3
+      native-comp-async-report-warnings-errors nil
       )
 
 ;; To speedup the Emacs windows, reducing the count on searching `load-path'
